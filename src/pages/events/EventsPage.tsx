53bcad7effee40164f8fb9495517f536969e537e
Pranav-Talmale <pranavtalmale@gmail.com>
2024-07-19 14:18:36 +0530

    Fix Build Error

--- src/pages/events/EventsPage.tsx
+++ src/pages/events/EventsPage.tsx
@@ -142,7 +142,7 @@ export function EventsPage() {
         ) : null}
       </AnimatePresence>
       <ul className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {cardContent.map((card, index) => (
+        {cardContent.map((card) => (
           <motion.div
             layoutId={`card-${card.id}`}
             key={card.id}
