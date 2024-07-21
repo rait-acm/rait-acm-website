7355fdbd70e85875fc16fd92220b64ff870c1c1e
Pranav-Talmale <pranavtalmale@gmail.com>
2024-07-21 21:09:37 +0530

    Change and replace icons for Domains Section

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -3,7 +3,7 @@ import { Feature } from "@/types/feature";
 const featuresData: Feature[] = [
   {
     id: 1,
-    icon: "/images/icon/icon-01.svg",
+    icon: "/images/icon/icon-code.svg",
     title: "Technical",
     description:
       "The technical team organizes technical events and activities. Assuring the website is updated and providing technical assistance is the team's responsibility.",
@@ -17,28 +17,28 @@ const featuresData: Feature[] = [
   },
   {
     id: 3,
-    icon: "/images/icon/icon-03.svg",
+    icon: "/images/icon/icon-editorial.svg",
     title: "Editorial",
     description:
       "Our editorial team composes the blog and encourages people to read it. The team produces high quality research articles.",
   },
   {
     id: 4,
-    icon: "/images/icon/icon-04.svg",
+    icon: "/images/icon/icon-publicity.svg",
     title: "Publicity",
     description:
       "Our publicity team promotes our events and workshops on social media platforms. They inform the audience why they should register and the benefits they will receive.",
   },
   {
     id: 5,
-    icon: "/images/icon/icon-05.svg",
+    icon: "/images/icon/icon-creative.svg",
     title: "Creative",
     description:
       "The creative team creates posters for all events. During Techfest, the creative team also decorates the department floor.",
   },
   {
     id: 6,
-    icon: "/images/icon/icon-06.svg",
+    icon: "/images/icon/icon-media-graphics.svg",
     title: "Media Graphics",
     description:
       "Videos are produced for events by the video editing team. In addition, the team makes sure that the online events are properly recorded and uploaded to the YouTube channel for further reference.",
