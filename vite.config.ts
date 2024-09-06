09596ec9f2f595b4185d56252d2193e017b4ba30
Pranav-Talmale <pranavtalmale@gmail.com>
2024-09-06 14:06:17 +0530

    Optimize Production JS Bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Inspect from "vite-plugin-inspect";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Inspect()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname), // Set this to the top-level directory
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
     },
   },
 });
